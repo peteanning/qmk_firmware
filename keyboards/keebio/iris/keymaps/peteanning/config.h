a5df1446be76dfca5696e660157d403a8151d777
Peter Anning <22272449+peteanning@users.noreply.github.com>
2026-08-25 10:13:24 +0100

    fix iris rev2 keymap build: drop RGBLED_NUM/RGBLIGHT_ANIMATIONS now provided by keyboard.json

--- keyboards/keebio/iris/keymaps/peteanning/config.h
+++ keyboards/keebio/iris/keymaps/peteanning/config.h
@@ -28,9 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 12
+/* LED count and animations now come from keyboards/keebio/iris/rev2/keyboard.json */
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
